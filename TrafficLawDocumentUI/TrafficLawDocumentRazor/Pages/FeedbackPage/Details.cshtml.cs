f9db3917ec248aaa19d7f9d0734623d3aa2cb91e
DuongDucManh <manhmanhd0402@gmail.com>
2025-07-24 19:05:23 +0700

    Remove DeleteFeedback page

--- TrafficLawDocumentUI/TrafficLawDocumentRazor/Pages/FeedbackPage/Details.cshtml.cs
+++ TrafficLawDocumentUI/TrafficLawDocumentRazor/Pages/FeedbackPage/Details.cshtml.cs
@@ -1,11 +1,4 @@
-﻿using BussinessObject;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Util.DTOs.ApiResponse;
 using Util.DTOs.FeedbackDTOs;
 
