88b012a781b81a13ee480dc835c6f7c7152073f9
Phan Pham Hoa <hoappse171730@fpt.edu.vn>
2025-07-26 16:58:24 +0700

    fix: Bug DM05

--- TrafficLawDocumentUI/Util/DTOs/LawDocumentDTOs/BaseLawDocumentDTO.cs
+++ TrafficLawDocumentUI/Util/DTOs/LawDocumentDTOs/BaseLawDocumentDTO.cs
@@ -1,10 +1,15 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Util.DTOs.LawDocumentDTOs
 {
     public class BaseLawDocumentDTO
     {
+        [Required(ErrorMessage = "Title is required")]
         public string? Title { get; set; }
+        [Required(ErrorMessage = "Document code is required")]
         public string? DocumentCode { get; set; }
+        [Required(ErrorMessage = "Category is required")]
         public Guid? CategoryId { get; set; }
         public string? FilePath { get; set; }
         public string? LinkPath { get; set; }
